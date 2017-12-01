28d9af6d98b93f53257de8a5596d27e8546f9afa
Vik Fearing <vik.fearing@protonmail.com>
2017-12-01 10:46:56 +0100

    Clean up Day01 a little bit

--- Day01/script.sql
+++ Day01/script.sql
@@ -2,25 +2,23 @@ CREATE TABLE day01 (input text);
 
 \COPY day01 FROM 'input.txt'
 
-SELECT coalesce(sum(digit) FILTER (WHERE digit = next_digit), 0) AS first_star
+SELECT coalesce(sum(digit::integer) FILTER (WHERE digit = next_digit), 0) AS first_star
 FROM (
-    SELECT u.digit::integer,
-           coalesce(lead(u.digit) OVER w, first_value(u.digit) OVER w)::integer AS next_digit
+    SELECT u.digit,
+           coalesce(lead(u.digit) OVER w, first_value(u.digit) OVER w) AS next_digit
     FROM day01 AS d,
          unnest(string_to_array(d.input, null)) WITH ORDINALITY AS u (digit, ordinality)
     WINDOW w AS (ORDER BY u.ordinality)
 ) _;
 
-
-SELECT sum(digit) FILTER (WHERE ordinality <= length(input) AND digit = next_digit) AS second_star
+SELECT coalesce(sum(digit::integer) FILTER (WHERE ordinality <= length(input) AND digit = next_digit), 0) AS second_star
 FROM (
     SELECT d.input,
-           u.digit::integer,
+           u.digit,
            u.ordinality,
-           coalesce(lead(u.digit, length(d.input)/2) OVER w)::integer AS next_digit
+           coalesce(lead(u.digit, length(d.input)/2) OVER (ORDER BY u.ordinality)) AS next_digit
     FROM day01 AS d,
          unnest(string_to_array(d.input || d.input, null)) WITH ORDINALITY AS u (digit, ordinality)
-    WINDOW w AS (ORDER BY u.ordinality)
 ) _;
 
 DROP TABLE day01;
