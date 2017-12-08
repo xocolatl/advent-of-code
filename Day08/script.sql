66e361e2f54b4001344558e38949b74757bca68a
Vik Fearing <vik.fearing@protonmail.com>
2017-12-08 22:19:27 +0100

    Simplify Day 8's interpreter quite a bit

--- Day08/script.sql
+++ Day08/script.sql
@@ -4,21 +4,24 @@ CREATE TABLE day08 (rownum serial, input text);
 
 WITH RECURSIVE
 input AS (
-SELECT d.rownum,
-       match[1] AS register,
-       match[2] AS op,
-       match[3]::integer AS diff,
-       match[4] AS if_register,
-       match[5] AS if_op,
-       match[6]::integer AS if_value
-FROM day08 AS d,
-     regexp_match(d.input, '^(\w+) (\w+) (-?\d+) if (\w+) ([!=<>]+) (-?\d+)$') AS match
+    SELECT d.rownum,
+           match[1] AS register,
+           CASE match[2]
+               WHEN 'inc' THEN +1
+               WHEN 'dec' THEN -1
+           END AS sign,
+           match[3]::integer AS diff,
+           match[4] AS if_register,
+           match[5] AS if_op,
+           match[6]::integer AS if_value
+    FROM day08 AS d,
+         regexp_match(d.input, '^(\w+) (\w+) (-?\d+) if (\w+) ([!=<>]+) (-?\d+)$') AS match
 ),
 
 loop (rownum, register, op, diff, if_register, if_op, if_value, memory) AS (
     SELECT 0,
            null::text,
-           null::text,
+           null::integer,
            null::integer,
            null::text,
            null::text,
@@ -27,56 +30,20 @@ loop (rownum, register, op, diff, if_register, if_op, if_value, memory) AS (
     UNION ALL
     SELECT i.rownum,
            i.register,
-           i.op,
+           i.sign,
            i.diff,
            i.if_register,
            i.if_op,
            i.if_value,
            l.memory || jsonb_build_object(i.register,
-               CASE i.if_op
-                   WHEN '<=' THEN
-                       CASE WHEN coalesce((l.memory->>i.if_register)::integer, 0) <= i.if_value THEN
-                           CASE i.op WHEN 'inc' THEN coalesce((l.memory->>i.register)::integer, 0) + i.diff
-                                     WHEN 'dec' THEN coalesce((l.memory->>i.register)::integer, 0) - i.diff
-                           END
-                       ELSE coalesce((l.memory->>i.register)::integer, 0)
-                       END
-                   WHEN '<' THEN
-                       CASE WHEN coalesce((l.memory->>i.if_register)::integer, 0) < i.if_value THEN
-                           CASE i.op WHEN 'inc' THEN coalesce((l.memory->>i.register)::integer, 0) + i.diff
-                                     WHEN 'dec' THEN coalesce((l.memory->>i.register)::integer, 0) - i.diff
-                           END
-                       ELSE coalesce((l.memory->>i.register)::integer, 0)
-                       END
-                   WHEN '!=' THEN
-                       CASE WHEN coalesce((l.memory->>i.if_register)::integer, 0) <> i.if_value THEN
-                           CASE i.op WHEN 'inc' THEN coalesce((l.memory->>i.register)::integer, 0) + i.diff
-                                     WHEN 'dec' THEN coalesce((l.memory->>i.register)::integer, 0) - i.diff
-                           END
-                       ELSE coalesce((l.memory->>i.register)::integer, 0)
-                       END
-                   WHEN '==' THEN
-                       CASE WHEN coalesce((l.memory->>i.if_register)::integer, 0) = i.if_value THEN
-                           CASE i.op WHEN 'inc' THEN coalesce((l.memory->>i.register)::integer, 0) + i.diff
-                                     WHEN 'dec' THEN coalesce((l.memory->>i.register)::integer, 0) - i.diff
-                           END
-                       ELSE coalesce((l.memory->>i.register)::integer, 0)
-                       END
-                   WHEN '>' THEN
-                       CASE WHEN coalesce((l.memory->>i.if_register)::integer, 0) > i.if_value THEN
-                           CASE i.op WHEN 'inc' THEN coalesce((l.memory->>i.register)::integer, 0) + i.diff
-                                     WHEN 'dec' THEN coalesce((l.memory->>i.register)::integer, 0) - i.diff
-                           END
-                       ELSE coalesce((l.memory->>i.register)::integer, 0)
-                       END
-                   WHEN '>=' THEN
-                       CASE WHEN coalesce((l.memory->>i.if_register)::integer, 0) >= i.if_value THEN
-                           CASE i.op WHEN 'inc' THEN coalesce((l.memory->>i.register)::integer, 0) + i.diff
-                                     WHEN 'dec' THEN coalesce((l.memory->>i.register)::integer, 0) - i.diff
-                           END
-                       ELSE coalesce((l.memory->>i.register)::integer, 0)
-                       END
-               END)
+               coalesce((l.memory->>i.register)::integer, 0) + i.sign * coalesce(CASE
+                   WHEN i.if_op = '<=' AND coalesce((l.memory->>i.if_register)::integer, 0) <= i.if_value THEN i.diff
+                   WHEN i.if_op = '<'  AND coalesce((l.memory->>i.if_register)::integer, 0) <  i.if_value THEN i.diff
+                   WHEN i.if_op = '!=' AND coalesce((l.memory->>i.if_register)::integer, 0) <> i.if_value THEN i.diff
+                   WHEN i.if_op = '==' AND coalesce((l.memory->>i.if_register)::integer, 0) =  i.if_value THEN i.diff
+                   WHEN i.if_op = '>'  AND coalesce((l.memory->>i.if_register)::integer, 0) >  i.if_value THEN i.diff
+                   WHEN i.if_op = '>=' AND coalesce((l.memory->>i.if_register)::integer, 0) >= i.if_value THEN i.diff
+                   END, 0))
     FROM loop AS l
     JOIN input AS i on i.rownum = l.rownum+1
 )
