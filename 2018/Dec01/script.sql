af81bd503432a2aca4b8cca8b6490caf08697154
Vik Fearing <vik.fearing@2ndquadrant.com>
2018-12-01 21:28:37 +0100

    We can be smarter about our solution and then SQL works fine.

--- 2018/Dec01/script.sql
+++ 2018/Dec01/script.sql
@@ -11,45 +11,24 @@ SELECT sum(input)
 FROM dec01;
 
 /*
- * PostgreSQL does not (yet) optimize for tail recursion so we have to resort
- * to plpgsql to have any kind of space management.
+ * Part Two is a little bit more involved.
  *
- * At first I did this the naive way by double looping and storing everything
- * seen in an array, but that took about three minutes.  Since we're not doing
- * pure SQL anyway, let's use a table so we can take advantage of a UNIQUE
- * btree index.  Unfortunately, that means we have to pattern match the error
- * message to get the value.
- *
- * This version executes in about half a second.
+ * Instead of looping over everything, we can just use modular arithmetic.
+ * Each iteration in the looping solution will add the sum found in Part One to
+ * each value.  We can just look for pairs of sums that are multiples of that
+ * apart.  Then we take the one with the fewest of those multiples.  Then we
+ * just need to make sure we find the first one in the list with that property.
  */
-DO LANGUAGE plpgsql
-$$
-DECLARE
-    detail text;
-    step CONSTANT bigint NOT NULL DEFAULT (SELECT sum(input) FROM dec01);
-    i bigint NOT NULL DEFAULT 0;
-BEGIN
-    CREATE TEMPORARY TABLE freqs (freq bigint PRIMARY KEY);
-
-    LOOP
-        INSERT INTO freqs
-            SELECT i + sum(input) OVER (ORDER BY rownum)
-            FROM dec01
-            ORDER BY rownum;
-        i := i + step;
-    END LOOP;
-EXCEPTION
-    /* How ugly is this, eh? */
-    WHEN unique_violation THEN
-        GET STACKED DIAGNOSTICS detail = PG_EXCEPTION_DETAIL;
-        RAISE NOTICE '%', substring(detail from 'Key \(freq\)=\((\d+)\) already exists.');
-END;
-$$;
-
-/*
- * Wish list:
- *
- * -   Tail recursion optimization
- * -   Some sort of "set" type that can efficiently find a value
- *
- */
+WITH
+sums (rownum, sum) AS (
+    SELECT rownum, sum(input) OVER (ORDER BY rownum)
+    FROM dec01
+)
+SELECT a.sum
+FROM sums AS a
+JOIN sums AS b
+    ON a.rownum <> b.rownum
+   AND a.sum > b.sum
+   AND (a.sum - b.sum) % (SELECT sum(input) FROM dec01) = 0
+ORDER BY a.sum - b.sum, b.rownum
+LIMIT 1;
